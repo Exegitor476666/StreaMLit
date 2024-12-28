1cbbc5bfe4036684c801df7ccb3491e6105a85c6
Exegitor476666 <111403334+Exegitor476666@users.noreply.github.com>
2024-12-28 09:37:44 +0000

    lmao

--- main.py
+++ main.py
@@ -1,4 +1,37 @@
 import streamlit as st
-st.title("rizz >:)")
-st.write("hi lol")
-btn = st.button("click me")
+import requests
+
+responses = requests.get("https://randomuser.me/api/")
+
+data = responses.json()
+
+thumbnail = data["results"][0]["picture"]["medium"]
+
+last_name = data["results"][0]["name"]["last"]
+first_name = data["results"][0]["name"]["first"]
+
+gender = data["results"][0]["gender"].title()
+
+email = data["results"][0]["email"]
+
+timezone = data["results"][0]["location"]["timezone"]["offset"]
+city = data["results"][0]["location"]["city"]
+state = data["results"][0]["location"]["state"]
+country = data["results"][0]["location"]["country"]
+postcode = data["results"][0]["location"]["postcode"]
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image(thumbnail, caption=f"{first_name} {last_name}", width=200)
+
+    st.write(
+        f"""
+Name: {first_name} {last_name}\n
+Gender: {gender}\n
+Email: {email}\n
+Lives in: {city}, {state}, {country}\n
+Postcode: {postcode}\n
+Timezone: GMT {timezone}
+"""
+)
